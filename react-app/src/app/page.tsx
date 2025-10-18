6ce260808accdddb8491d06af8a7cf8f469d665d
llysi <zhaoalysa2006@gmail.com>
2025-10-18 18:26:30 -0500

    Wrap title and subtitle in a div

--- react-app/src/app/page.tsx
+++ react-app/src/app/page.tsx
@@ -2,23 +2,25 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      {/* Title */}
-      <h1 className="text-center text-xl">
-        Job RoadMap Generator
-      </h1>
+    <div className="min-h-screen flex flex-col items-center justify-between px-4">
+      <div>
+        {/* Title */}
+        <h1 className="text-center text-xl ">
+          Job RoadMap Generator
+        </h1>
 
-      {/* Subtitle */}
-      <p className="text-center text-l">
-        Get unemployed with AI learning paths
-      </p>
+        {/* Subtitle */}
+        <p className="text-center text-l">
+          Get unemployed with AI learning paths
+        </p>
+      </div>
 
-      {/* Call to Action */}
-      <Link href="/form">
-        <button>
-          Start Your Journey →
-        </button>
-      </Link>
+        {/* Call to Action */}
+        <Link href="/form">
+          <button>
+            Start Your Journey →
+          </button>
+        </Link>
     </div>
   );
 }
