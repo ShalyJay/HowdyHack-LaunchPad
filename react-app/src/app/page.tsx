e3c942203e65eb900a881bbbe2266cf70e835504
llysi <zhaoalysa2006@gmail.com>
2025-10-18 18:19:19 -0500

    Create basic landing page placeholder text (Title, about text, action button)

--- react-app/src/app/page.tsx
+++ react-app/src/app/page.tsx
@@ -1,12 +1,24 @@
-'use client';
-
-import Image from 'next/image';
-import { useState } from 'react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      
+      {/* Title */}
+      <h1 className="text-center text-xl">
+        Job RoadMap Generator
+      </h1>
+
+      {/* Subtitle */}
+      <p className="text-center text-l">
+        Get unemployed with AI learning paths
+      </p>
+
+      {/* Call to Action */}
+      <Link href="/form">
+        <button>
+          Start Your Journey →
+        </button>
+      </Link>
     </div>
   );
 }
