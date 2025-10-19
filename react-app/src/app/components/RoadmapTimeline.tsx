93bf5ac43851f6b4aad390a086674b8f30c8ae74
llysi <zhaoalysa2006@gmail.com>
2025-10-19 00:01:24 -0500

    Add RoadmapTimeline component - displays module title, duration, skills,   and resources

--- react-app/src/app/components/RoadmapTimeline.tsx
+++ react-app/src/app/components/RoadmapTimeline.tsx
@@ -5,3 +5,49 @@ interface Module {
     description: string;
     resources: string[];
 }
+
+interface RoadmapTimelineProps {
+    modules: Module[];
+}
+
+// recieves array of module objects --> loops through modules to display stuff
+export default function RoadmapTimeline({ modules }: RoadmapTimelineProps) 
+{
+    // If no modules, don't show anything
+    if (!modules || modules.length === 0) {
+        return null;
+    }
+
+    return (
+        <div>
+            <h2>Your Learning Roadmap</h2>
+
+            {/* Loop through each module */}
+            {modules.map((module, index) => (
+                <div key={index}>
+                    <h3>{module.title}</h3>
+                    <p>Duration: {module.duration}</p>
+                    <p>{module.description}</p>
+
+                    <div>
+                        <p>Skills:</p>
+                        {module.skills.map((skill, skillIndex) => (
+                            <span key={skillIndex}>{skill} </span>
+                        ))}
+                    </div>
+
+                    {module.resources && module.resources.length > 0 && (
+                        <div>
+                            <p>Resources:</p>
+                            <ul>
+                                {module.resources.map((resource, resourceIndex) => (
+                                    <li key={resourceIndex}>{resource}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
+            ))}
+        </div>
+    );
+}
