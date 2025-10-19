fa6df80f73cb6a0d0ce7d7628276be5df39243f3
llysi <zhaoalysa2006@gmail.com>
2025-10-19 00:03:36 -0500

    import RoadmapTimeline component

--- react-app/src/app/form/page.tsx
+++ react-app/src/app/form/page.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
+import RoadmapTimeline from '../components/RoadmapTimeline';
 
 export default function Form() {
     // form input states
